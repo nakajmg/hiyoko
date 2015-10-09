43d552ae6f082ce87c517575b06a932d97b232ef
nakajmg <nakajima.jmg@gmail.com>
2015-10-09 11:38:46 +0900

    add list component

---
+++ js/component/menu-list.js
@@ -0,0 +1,19 @@
+module.exports = {
+  data() {
+
+  },
+  template: `
+    <ul class="m-menu">
+      <li class="m-menu__item">
+        <a><span class="m-menu__newPost"></span><span>NEW POST</span></a>
+      </li>
+      <li class="m-menu__item">
+        <a><i class="fa fa-file-text-o"></i><span>POSTS</span></a>
+      </li>
+      <li class="m-menu__item --spacer"></li>
+      <li class="m-menu__item">
+        <a><i class="fa fa-gear"></i><span>SETTING</span></a>
+      </li>
+    </ul>
+  `
+};
