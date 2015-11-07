8edab9891206880e90befc3291a30361d7742ef2
nakajmg <nakajima.jmg@gmail.com>
2015-11-07 13:23:15 +0900

    エディタに通知モジュールを追加

--- js/editor.js
+++ js/editor.js
@@ -6,9 +6,13 @@
   Vue.component("hiyoko-editor", require("./js/component/hiyoko-editor"));
   Vue.component("hiyoko-preview", require("./js/component/hiyoko-preview"));
   Vue.component("hiyoko-toolbar", require("./js/component/hiyoko-toolbar"));
+  Vue.component("dialog-notify", require("./js/component/dialog-notify"));
+  Vue.component("dialog-loader", require("./js/component/dialog-loader"));
+  Vue.component("dialog-input", require("./js/component/dialog-input"));
+  Vue.component("dialog-confirm", require("./js/component/dialog-confirm"));
 
   function init(currentPost) {
-    vm = new Vue({
+    window.vm = new Vue({
       el: "#app",
       data: {
         currentPost: currentPost,
@@ -23,6 +27,24 @@
       events: {
         "update:editor"() {
           this.update();
+        },
+        "show:dialog:input"(options) {
+          this.$refs.dialoginput.$emit("show", options);
+        },
+        "close:dialog:input"(value) {
+          this.input = value;
+        },
+        "show:dialog:confirm"(options) {
+          this.$refs.dialogconfirm.$emit("show", options);
+        },
+        "show:dialog:loader"() {
+          this.$refs.dialogloader.$emit("show");
+        },
+        "close:dialog:loader"() {
+          this.$refs.dialogloader.$emit("close");
+        },
+        "add:dialog:notify"(notify) {
+          this.$refs.dialognotify.$emit("add", notify);
         }
       },
 
